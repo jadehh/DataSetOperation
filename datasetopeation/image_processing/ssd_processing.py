3f81ce1bf14f421126fb9657bcef645b7dde57e8
jade <jadehh@live.com>
2019-08-21 17:49:14 +0800

    tf.data 制作数据集

---
+++ datasetopeation/image_processing/ssd_processing.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# 作者：2019/8/15 by jade
+# 邮箱：jadehh@live.com
+# 描述：TODO
+# 最近修改：2019/8/15  下午2:05 modify by jade
+import tensorflow as tf
+
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.84
+
+
+_RESIZE_SIDE_MIN = 256
+_RESIZE_SIZE_MAX = 512
+
+def _crop(image,offset_height,offset_width,crop_height,crop_width):
+    """
+    裁剪图片
+    :param image:
+    :param offset_height:
+    :param offset_width:
+    :param crop_height:
+    :param crop_width:
+    :return:
+    """
+    orignal_shape = tf.shape(image)
+    rank_assertion = tf.Assert(
+        tf.equal(tf.rank(image),3),
+        ["图片的rank 必须等于 3"]
+    )
+    with tf.control_dependencies([rank_assertion]):
+        cropped_shape = tf.stack([crop_height,crop_width,orignal_shape[2]])
+
+    size_assertion = tf.Assert(
+        tf.logical_and(
+            tf.greater_equal(orignal_shape[0],crop_height),
+            tf.greater_equal(orignal_shape[1],crop_width),
+        ),
+        ["被裁剪的图片宽高必须大于裁剪的宽和高"]
+    )
+    offsets = tf.cast(tf.stack([offset_height,offset_width,0]),dtype=tf.int32)
+    with tf.control_dependencies([size_assertion]):
+        image = tf.slice(image,offsets,cropped_shape)
+    return tf.reshape(image,cropped_shape)
+
+def _central_crop(image_list, crop_height, crop_width):
+  """Performs central crops of the given image list.
+
+  Args:
+    image_list: a list of image tensors of the same dimension but possibly
+      varying channel.
+    crop_height: the height of the image following the crop.
+    crop_width: the width of the image following the crop.
+
+  Returns:
+    the list of cropped images.
+  """
+  outputs = []
+  for image in image_list:
+    image_height = tf.shape(image)[0]
+    image_width = tf.shape(image)[1]
+
+    offset_height = (image_height - crop_height) / 2
+    offset_width = (image_width - crop_width) / 2
+
+    outputs.append(_crop(image, offset_height, offset_width,
+                         crop_height, crop_width))
+  return outputs
+
+
+def _smallest_size_at_least(height,width,smallest_side):
+    """
+    计算最小边等于smallest_side 的 新形状
+    :param height: 当前图像的高
+    :param width: 当前图像的宽
+    :param smallest_side: python 的整数 或者 tensor ,调整大小后的最小边
+    :return: 新的高和宽
+    """
+
+    smallest_side = tf.convert_to_tensor(smallest_side,dtype=tf.int32)
+    height = tf.cast(height,tf.float32)
+    width = tf.cast(width,tf.float32)
+    smallest_side = tf.cast(smallest_side,tf.float32)
+    scale = tf.cond(tf.greater(height,width),
+                    lambda: smallest_side / width,
+                    lambda: smallest_side / height)  #smallest_side / 最小的边
+
+    new_height = tf.cast(height * scale,tf.int32)
+    new_width = tf.cast(width * scale,tf.int32)
+
+    return new_height,new_width
+
+def _aspect_preserving_resize(image,smallest_side):
+    """
+    调整图像大小，保持原本的图像纵横比
+    :param image: image tensor
+    :param smallest_side: python 的整数 或者 tensor ,调整大小后的最小边
+    :return: 处理后的图像
+    """
+    smallest_side = tf.convert_to_tensor(smallest_side,dtype=tf.int32)
+    shape = tf.shape(image)
+    height = shape[0]
+    width = shape[1]
+
+    new_height,new_width = _smallest_size_at_least(height,width,smallest_side)
+    image = tf.expand_dims(image,0)
+    resized_image = tf.image.resize(image,[new_height,new_width])
+    resized_image = tf.squeeze(resized_image)
+    resized_image.set_shape([None,None,3])
+    return resized_image
+
+
+def _mean_image_subtraction(image,means):
+    """
+    从每个图像通道中减去给定的平均值
+    :param image: 图像
+    :param means: 均值
+    :return: 减去均值后的图像
+    """
+    if image.get_shape().ndims !=3 :
+        raise ValueError("图像的通道数必须为3")
+    num_channels = image.get_shape().as_list()[-1]
+    if len(means) != num_channels:
+        raise ValueError("均值列表必须等于通道数")
+
+    channels = tf.split(axis=2,num_or_size_splits=num_channels,value=image)
+    for i in range(num_channels):
+        channels[i] -= means[i]
+    return tf.concat(axis=2,values=channels)
+
+def _random_crop(image_list,crop_height,crop_width):
+    """
+    裁剪给定的图片列表
+    :param image_list: 图片列表
+    :param crop_height: 生成图片的高
+    :param crop_width: 生成图片的宽
+    :return: 裁剪后的图片
+    """
+    if not image_list:
+        raise ValueError("图片列表为空")
+
+    rank_arrsertions = []
+    for i in range(len(image_list)):
+        image_rank = tf.rank(image_list[i])
+        rank_assert = tf.Assert(
+            tf.equal(image_rank,3),
+            ["Wrong rank for tensor %d [expected] [actual]",i,3,image_rank])
+        rank_arrsertions.append(rank_assert)
+
+    with tf.control_dependencies([rank_arrsertions[0]]):
+        image_shape = tf.shape(image_list[0])
+
+    image_height = image_shape[0]
+    image_width = image_shape[1]
+    crop_size_assert = tf.Assert(
+        tf.logical_and(
+            tf.greater_equal(image_height,crop_height),
+            tf.greater_equal(image_width,crop_width)),
+            ["Crop size greather than the image size"]
+    )
+
+    asserts = [rank_arrsertions[0],crop_size_assert]
+    for i in range(1,len(image_list)):
+        image = image_list[i]
+        asserts.append(rank_arrsertions[i])
+        with tf.control_dependencies([rank_arrsertions[i]]):
+            shape = tf.shape(image)
+        height = shape[0]
+        width = shape[1]
+
+        height_assert = tf.Assert(
+            tf.equal(height,image_height),
+            ["Wrong height for tensor %s [expected] [actual]",
+             image.name,height,image_height]
+        )
+        width_assert = tf.Assert(
+            tf.equal(height,image_height),
+            ["Wrong width for tensor %s [expected] [actual]",
+             image.name, width, image_width]
+        )
+
+        asserts.extend([height_assert,width_assert])
+
+    with tf.control_dependencies(asserts):
+        max_offset_height = tf.reshape(image_height-crop_height + 1,[])
+    with tf.control_dependencies(asserts):
+        max_offset_width = tf.reshape(image_width - crop_width + 1, [])
+
+    offset_height = tf.random.uniform([],
+                                      maxval=max_offset_height,
+                                      dtype=tf.int32)
+
+    offset_width = tf.random.uniform([],
+                                     maxval=max_offset_width,
+                                     dtype=tf.int32)
+
+    return [_crop(image,offset_height,offset_width,crop_height,crop_height) for image in image_list]
+
+
+def process_for_train(image,
+                      output_height,
+                      output_width,
+                      is_train=True,
+                      resize_side_min = _RESIZE_SIDE_MIN,
+                      resize_side_max = _RESIZE_SIZE_MAX):
+    """
+    训练数据图像预处理
+    :param image:
+    :param output_height: 输出图像的高
+    :param output_width: 输出图像的宽
+    :param resize_side_min: 预处理的resize最小的值
+    :param resize_side_max: 预处理的resize最大的值
+    :return: 预处理后的图像
+    """
+    image = tf.image.resize(image,[output_height,output_width])
+    image = tf.cast(image,tf.float32)
+    # image = tf.image.random_flip_left_right(image)
+    image.set_shape([None, None, 3])
+    return image
+    #return _mean_image_subtraction(image,[_R_MEAN,_G_MEAN,_B_MEAN])
+
+def process_for_eval(image,output_height,output_width,resize_side_min=_RESIZE_SIDE_MIN):
+    image = tf.image.resize(image,[output_height,output_width])
+    image = tf.cast(image,tf.float32)
+    image = tf.image.random_flip_left_right(image)
+    image.set_shape([None, None, 3])
+    return _mean_image_subtraction(image,[_R_MEAN,_G_MEAN,_B_MEAN])
+
+
+if __name__ == '__main__':
+    import cv2
+    image1 = cv2.imread("/home/jade/Data/VOCdevkit/Classify/aeroplane/2007_000032_0.jpg")
+    cv2.imshow("result1",image1)
+    for i in range(10):
+        image = tf.convert_to_tensor(image1)
+        image = process_for_train(image,224,224)
+        cv2.imshow("result_"+str(i),image.numpy().astype("uint8"))
+    cv2.waitKey(0)
+    print(image.numpy())
--- datasetopeation/jadeVocTFRecord.py
+++ datasetopeation/jadeVocTFRecord.py
@@ -17,17 +17,21 @@ import os
 from lxml import etree
 import PIL.Image
 import tensorflow as tf
+from tensorflow.python.platform.gfile import GFile
 from object_detection.utils import dataset_util
 import argparse
 from jade import *
+from datasetopeation.image_processing.ssd_processing import process_for_train, process_for_eval
+
 
 def dict_voc_to_tf_example(data,
-                           dataset_directory,
-                           label_map_dict,
+                           voc_dir,
+                           class_names,
                            xml_name,
                            year,
                            ignore_difficult_instances=False,
-                           image_subdirectory='JPEGImages'
+                           image_subdirectory='JPEGImages',
+                           is_normal=True,
                            ):
     """
     转换成tf格式
@@ -40,10 +44,10 @@ def dict_voc_to_tf_example(data,
     :param image_subdirectory:
     :return:
     """
-    data['folder'] = dataset_directory + year
+    data['folder'] = voc_dir
     img_path = os.path.join(data['folder'], image_subdirectory, GetLastDir(xml_name)[:-4] + '.jpg')
-    full_path = os.path.join(dataset_directory, img_path)
-    with tf.gfile.GFile(full_path, 'rb') as fid:
+    full_path = img_path
+    with GFile(full_path, 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = PIL.Image.open(encoded_jpg_io)
@@ -70,119 +74,218 @@ def dict_voc_to_tf_example(data,
             # continue
 
             difficult_obj.append(int(0))
-            xmin.append(float(obj['bndbox']['xmin']) / width)
-            ymin.append(float(obj['bndbox']['ymin']) / height)
-            xmax.append(float(obj['bndbox']['xmax']) / width)
-            ymax.append(float(obj['bndbox']['ymax']) / height)
-            classes_text.append(obj['name'].encode('utf8'))
-            classes.append(int(label_map_dict[obj['name']]["id"]))
-            truncated.append(0)
-            obj['pose'] = "Unspecified"
-            poses.append(obj['pose'].encode('utf8'))
-
-    example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(
-            data['filename'].encode('utf8')),
-        'image/source_id': dataset_util.bytes_feature(
-            data['filename'].encode('utf8')),
-        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-        'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
-        'image/object/truncated': dataset_util.int64_list_feature(truncated),
-        'image/object/view': dataset_util.bytes_list_feature(poses),
-    }))
+            if int(class_names.index(obj['name'])) > 0:
+                if is_normal:
+                    xmin.append(float(obj['bndbox']['xmin']) / width)
+                    ymin.append(float(obj['bndbox']['ymin']) / height)
+                    xmax.append(float(obj['bndbox']['xmax']) / width)
+                    ymax.append(float(obj['bndbox']['ymax']) / height)
+                else:
+                    xmin.append(int(obj['bndbox']['xmin']))
+                    ymin.append(int(obj['bndbox']['ymin']))
+                    xmax.append(int(obj['bndbox']['xmax']))
+                    ymax.append(int(obj['bndbox']['ymax']))
+                classes_text.append(obj['name'].encode('utf8'))
+                classes.append(int(class_names.index(obj['name'])))
+                truncated.append(0)
+                obj['pose'] = "Unspecified"
+                poses.append(obj['pose'].encode('utf8'))
+    if is_normal:
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/source_id': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+            'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes),
+            'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+            'image/object/truncated': dataset_util.int64_list_feature(truncated),
+            'image/object/view': dataset_util.bytes_list_feature(poses),
+        }))
+    else:
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/source_id': dataset_util.bytes_feature(
+                data['filename'].encode('utf8')),
+            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+            'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+            'image/object/bbox/xmin': dataset_util.int64_list_feature(xmin),
+            'image/object/bbox/xmax': dataset_util.int64_list_feature(xmax),
+            'image/object/bbox/ymin': dataset_util.int64_list_feature(ymin),
+            'image/object/bbox/ymax': dataset_util.int64_list_feature(ymax),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes),
+            'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+            'image/object/truncated': dataset_util.int64_list_feature(truncated),
+            'image/object/view': dataset_util.bytes_list_feature(poses),
+        }))
     return example
 
 
+"""
+    years = []
+    root_dir = "/home/jade/Data/Hand_Gesture/"
+    for year in os.listdir(root_dir):
+        if year != "tfrecords" and os.path.isdir(os.path.join(root_dir, year)):
+            years.append(year)
+            dataset_name = os.path.join(root_dir, year)
+            CreateVOCDataset(dataset_name, "HandGesture")
+    paraser = argparse.ArgumentParser(description="Create TFRecords")
+    paraser.add_argument("--data_dir", default=root_dir, help="")
+    paraser.add_argument("--output_path", default=root_dir + "tfrecords/hand_gesture_train_" + GetToday() + ".tfrecord",
+                         help="")
+    paraser.add_argument("--proto_txt_path", default=root_dir + "hand_gesture.prototxt", help="")
+    paraser.add_argument("--years", type=list, default=years, help="")
+    args = paraser.parse_args()
+    CreateVOCTFRecords(args,True)
+"""
 
-def CreateVOCTFRecords(args):
-    CreateSavePath(GetPreviousDir(args.output_path))
-    data_dir = args.data_dir
-    CreateSavePath(GetPreviousDir(args.output_path))
-    writer = tf.python_io.TFRecordWriter(args.output_path)
-    label_map_dict, _ = ReadProTxt(args.proto_txt_path, id=False)
-    for year in args.years:
-        logging.info('Reading from VOC %s dataset.', year)
-        examples_list = GetVOCTrainXmlPath(os.path.join(data_dir, year))
-        for idx, example in enumerate(examples_list):
-            if idx % 100 == 0:
-                print('On image %d of %d' % (idx, len(examples_list)))
-            with tf.gfile.GFile(example, 'r') as fid:
-                xml_str = fid.read()
-            xml = etree.fromstring(xml_str)
-            data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+def CreateVOCTFRecords(voc_path,datasetname,prototxt,isnormal=True):
 
-            tf_example = dict_voc_to_tf_example(data, args.data_dir, label_map_dict, example, year,
-                                                True)
-            writer.write(tf_example.SerializeToString())
-    writer.close()
+    train_output_path = CreateSavePath(os.path.join(GetPreviousDir(voc_path), "TFRecords"))
+    train_output_path = train_output_path + "/" + datasetname + "_train.tfrecord"
+    train_writer = tf.io.TFRecordWriter(train_output_path)
+
+    test_output_path = CreateSavePath(os.path.join(GetPreviousDir(voc_path), "TFRecords"))
+    test_output_path = test_output_path + "/" + datasetname + "_test.tfrecord"
+    test_writer = tf.io.TFRecordWriter(test_output_path)
+
+    categorites,class_names = ReadProTxt(prototxt)
+    train_processBar = ProcessBar()
+    train_image_path = GetVOCTrainXmlPath(voc_path)
+    test_image_path = GetVOCTestXmlPath(voc_path)
+    train_processBar.count = len(train_image_path)
+    for idx, example in enumerate(train_image_path):
+        with GFile(example, 'r') as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+        train_processBar.start_time = time.time()
+        tf_example = dict_voc_to_tf_example(data, voc_path, class_names, example, datasetname,
+                                            True, is_normal=isnormal)
+        train_writer.write(tf_example.SerializeToString())
+        NoLinePrint("Writing train TFrecords ...",train_processBar)
+
+    print(" ")
+    test_processBar = ProcessBar()
+    test_processBar.count  = len(test_image_path)
+    for idx, example in enumerate(test_image_path):
+        with GFile(example, 'r') as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+        test_processBar.start_time = time.time()
+        tf_example = dict_voc_to_tf_example(data, voc_path, class_names, example, datasetname,
+                                            True, is_normal=isnormal)
+        test_writer.write(tf_example.SerializeToString())
+        NoLinePrint("Writing test TFrecords ...", test_processBar)
+    #
+    train_writer.close()
+    test_writer.close()
 
 
-def VOCTFRecordShow(tfrecord_path,proto_txt_path):
-    categories, _ = ReadProTxt(proto_txt_path)
-    with tf.Session() as sess:
-        example = tf.train.Example()
-        # train_records 表示训练的tfrecords文件的路径
-        record_iterator = tf.python_io.tf_record_iterator(path=tfrecord_path)
-        for record in record_iterator:
-            example.ParseFromString(record)
-            f = example.features.feature
-            # 解析一个example
-            # image_name = f['image/filename'].bytes_list.value[0]
-            image_encode = f['image/encoded'].bytes_list.value[0]
-            image_height = f['image/height'].int64_list.value[0]
-            image_width = f['image/width'].int64_list.value[0]
-            xmin = f['image/object/bbox/xmin'].float_list.value
-            ymin = f['image/object/bbox/ymin'].float_list.value
-            xmax = f['image/object/bbox/xmax'].float_list.value
-            ymax = f['image/object/bbox/ymax'].float_list.value
-            labels = f['image/object/class/label'].int64_list.value
-            text = f['image/object/class/text'].bytes_list.value
-            image = io.BytesIO(image_encode)
-            labels = list(labels)
-            xmin = list(xmin)
-            ymin = list(ymin)
-            xmax = list(xmax)
-            ymax = list(ymax)
-            image = Image.open(image)
+def parse_exmp(serial_exmp, output_height, output_width, is_train):
+    feats = tf.io.parse_single_example(serial_exmp,
+                                       features={'image/encoded': tf.io.FixedLenFeature([], tf.string),
+                                                 'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+                                                 'image/object/bbox/xmin':  tf.io.VarLenFeature(tf.float32),
+                                                 'image/object/bbox/xmax':  tf.io.VarLenFeature(tf.float32),
+                                                 'image/object/bbox/ymin':  tf.io.VarLenFeature(tf.float32),
+                                                 'image/object/bbox/ymax':  tf.io.VarLenFeature(tf.float32),
+                                                 })
 
-            image = np.asarray(image)
-            bboxes = []
-            scores = []
-            label_texts = []
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            for i in range(len(xmin)):
-                if labels[i] != 0:
-                    print(labels[i])
-                    bboxes.append(
-                        (xmin[i] * image_width, ymin[i] * image_height, xmax[i] * image_width, ymax[i] * image_height))
-                    scores.append(1)
-                    label_texts.append(categories[labels[i]]["display_name"])
-            print("**********************")
-            CVShowBoxes(image, bboxes, label_texts, labels, scores=scores, waitkey=0)
+    image = tf.cond(
+        tf.image.is_jpeg(feats['image/encoded']),
+        lambda: tf.image.decode_jpeg(feats['image/encoded']),
+        lambda: tf.image.decode_png(feats['image/encoded']))
+    label = feats['image/object/class/label']
+    label = tf.cast(label,tf.float32)
+    img = tf.cast(image, tf.float32)
+    xmin = feats['image/object/bbox/xmin']
+    xmax = feats['image/object/bbox/xmax']
+    ymin = feats['image/object/bbox/ymin']
+    ymax = feats['image/object/bbox/ymax']
+
+    label = tf.sparse.to_dense(label)
+    xmin = tf.sparse.to_dense(xmin)
+    xmax = tf.sparse.to_dense(xmax)
+    ymin = tf.sparse.to_dense(ymin)
+    ymax = tf.sparse.to_dense(ymax)
+
+    predict = tf.stack([label,xmin,xmax,ymin,ymax],axis=0)
+    # if is_train:
+    #     img = process_for_train(img, output_height, output_width)
+    # else:
+    #     img = process_for_eval(img, output_height, output_width)
+
+    return img,predict
+
+
+def get_dataset(fname, output_height, output_width, is_train):
+    dataset = tf.data.TFRecordDataset(fname)
+    return dataset.map(lambda serial_exmp: parse_exmp(serial_exmp, output_height, output_width, is_train))
+
+
+def LoadVOCTFRecord(tfrecord_path, batch_size=32, shuffle=True, repeat=True, output_height=300, output_width=300,
+                    is_train=True):
+    """
+    读取分类的TFrecords
+    :param tfrecord_path:
+    :param batch_size:
+    :param shuffle:
+    :param repeat:
+    :return:
+    """
+    dataset = get_dataset(tfrecord_path, output_height, output_width, is_train)
+    if shuffle:
+        dataset = dataset.shuffle(1000)
+    if repeat:
+        dataset = dataset.repeat()
+    dataset = dataset.batch(batch_size=batch_size)
+    return dataset
+
+
+def VOCTFRecordShow(tfrecord_path, proto_txt_path):
+    _,class_names = ReadProTxt(proto_txt_path)
+    train_ds = LoadVOCTFRecord(tfrecord_path,batch_size=1)
+
+    for (image,predicts) in train_ds:
+        boxes = []
+        scores = []
+        labels = []
+        label_text = []
+        image = image.numpy().astype('uint8')[0]
+        predicts = predicts.numpy()[0]
+        print(predicts)
+        for i in range(predicts.shape[1]):
+            label = predicts[0,i]
+            xmin = predicts[1,i]
+            xmax = predicts[2,i]
+            ymin = predicts[3,i]
+            ymax = predicts[4,i]
+            boxes.append([xmin,ymin,xmax,ymax])
+            scores.append(1)
+            labels.append(label)
+            label_text.append(class_names[int(label)])
+        CVShowBoxes(image,boxes,label_text,labels,scores,waitkey=0)
+
 
 
 if __name__ == '__main__':
-    root_dir = "/home/jade/Data/UAFaceGesture/checked/"
-    years = []
-    for year in os.listdir(root_dir):
-        if year != "tfrecords" and os.path.isdir(os.path.join(root_dir,year)):
-            years.append(year)
-    paraser = argparse.ArgumentParser(description="Create TFRecords")
-    paraser.add_argument("--data_dir",default=root_dir,help="")
-    paraser.add_argument("--output_path",default=root_dir+"tfrecords/face_gesture_train.tfrecord",help="")
-    paraser.add_argument("--proto_txt_path",default=root_dir+"face_gesture.prototxt",help="")
-    paraser.add_argument("--years",type=list,default=years,help="")
-    args = paraser.parse_args()
-    CreateVOCTFRecords(args)
-
-    VOCTFRecordShow("/home/jade/Data/UAFaceGesture/checked/tfrecords/face_gesture_train.tfrecord","/home/jade/Data/UAFaceGesture/checked/face_gesture.prototxt")
+    VOCTFRecordShow("/home/jade/Data/Hand_Gesture/TFRecords/UA_Handgesture_test.tfrecord","/home/jade/Data/Hand_Gesture/hand_gesture.prototxt")
+    train_ds = LoadVOCTFRecord("/home/jade/Data/Hand_Gesture/TFRecords/UA_Handgesture_test.tfrecord",batch_size=1)
+    for (image,label) in train_ds:
+        print(label.numpy())
